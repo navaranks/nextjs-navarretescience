2ced9ebb8dfc976f018851fa5cc38f498f320bd5
Chris <christophernavarrete00@gmail.com>
2024-07-14 19:56:00 -0700

    Removed dependency

--- components/schedule-table.tsx
+++ components/schedule-table.tsx
@@ -26,7 +26,7 @@ const SafeHtml: React.FC<SafeHtmlProps> = ({ html }) => {
         anchor.classList.add("text-primary", "hover:underline", "hover:decoration-wavy");
       });
     }
-  }, [sanitizedHtml]);
+  }, []);
 
   return (
     <span ref={spanRef} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
