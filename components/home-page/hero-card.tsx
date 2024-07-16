09b3a021ba888a1836191cbe61404739c3c6c66b
Chris <christophernavarrete00@gmail.com>
2024-07-16 13:04:09 -0700

    Added mov for iOS

--- components/home-page/hero-card.tsx
+++ components/home-page/hero-card.tsx
@@ -1,10 +1,18 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardContent } from '../ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
 function HeroCard() {
   const [videoLoaded, setVideoLoaded] = useState(false);
+  const [videoSrc, setVideoSrc] = useState('/images/logo2.webm');
+
+  useEffect(() => {
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !('MSStream' in window);
+    if (isIOS) {
+      setVideoSrc('/images/logo2.mov');
+    }
+  }, []);
 
   const handleVideoLoad = () => {
     setVideoLoaded(true);
@@ -15,7 +23,7 @@ function HeroCard() {
       <CardContent className='p-0'>
         {!videoLoaded && <Skeleton className="h-64 w-[600px] mx-auto" />}
         <video
-          src='/images/logo2.webm'
+          src={videoSrc}
           autoPlay
           muted
           playsInline
