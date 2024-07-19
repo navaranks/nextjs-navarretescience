339a9d95179415fb07a598739117bf221e06276d
Chris <christophernavarrete00@gmail.com>
2024-07-19 15:04:20 -0700

    Switched back to fetching

--- components/class-schedule.tsx
+++ components/class-schedule.tsx
@@ -5,22 +5,33 @@ import Schedule, { ScheduleData } from './schedule'; // Import Schedule and Sche
 import ScheduleCard from './schedule-card';
 
 interface ClassScheduleProps {
-  scheduleData: ScheduleData[]; // Accept scheduleData as a prop
+  apiURL: string;
 }
 
-export default function ClassSchedule({ scheduleData }: ClassScheduleProps) {
-    const [data, setData] = useState<ScheduleData[] | null>(null);
+export default function ClassSchedule({apiURL}: ClassScheduleProps) {
+    const [scheduleData, setScheduleData] = useState<ScheduleData[] | null>(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        setData(scheduleData);
-        setIsLoading(false);
-    }, [scheduleData]); // Dependency on scheduleData
+       // This will show what URL is received
+      if (!apiURL) return;
+      fetch(apiURL)
+          .then(response => response.json())
+          .then(data => {
+              setScheduleData(data);
+              setIsLoading(false);
+          })
+          .catch(error => {
+              console.error('Error fetching schedule:', error);
+              setIsLoading(false);
+          });
+  }, [apiURL]); // apiURL is a dependency here
+  
 
     return (
-        <main className='flex flex-col px-4 gap-y-2'>
+        <main className='flex flex-col px-4 gap-y-4'>
             <ScheduleCard />
-            <Schedule isLoading={isLoading} scheduleData={data}/>
+            <Schedule isLoading={isLoading} scheduleData={scheduleData}/>
         </main>
     );
 }
