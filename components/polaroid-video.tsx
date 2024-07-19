dff0a5eda8079f431fd5392e3779896d3dc19a13
Chris <christophernavarrete00@gmail.com>
2024-07-19 15:03:48 -0700

    Added iOS compatability

--- components/polaroid-video.tsx
+++ components/polaroid-video.tsx
@@ -1,24 +1,40 @@
-
-import { Card, CardContent, CardFooter } from "./ui/card";
+"use client";
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, CardFooter } from './ui/card';
 
 interface PolaroidProps {
   PolaroidImage: string;
   Caption: string;
-  
 }
 
+const isIOS = () => {
+  return /iPad|iPhone|iPod/.test(navigator.userAgent);
+};
 
 export default function PolaroidVideo({
   PolaroidImage,
   Caption,
 }: PolaroidProps) {
+  const [isIosDevice, setIsIosDevice] = useState(false);
 
+  useEffect(() => {
+    setIsIosDevice(isIOS());
+  }, []);
+
+  const getVideoSrc = (basename: string) => isIosDevice ? `${basename}.mp4` : `${basename}.webm`;
 
   return (
     <Card className="max-w-md md:max-w-sm w-full shadow-lg m-auto bg-muted">
-      <CardContent className="p-4 ">
-        <div className={`relative w-full overflow-hidden rounded-lg`}>
-          <video src={PolaroidImage} autoPlay muted loop playsInline   />
+      <CardContent className="p-4">
+        <div className="relative w-full overflow-hidden rounded-lg">
+          <video 
+            src={getVideoSrc(PolaroidImage)}
+            autoPlay 
+            muted 
+            loop 
+            playsInline 
+            className="w-full h-full"
+          />
         </div>
       </CardContent>
       <CardFooter className="block text-center text-pretty">
