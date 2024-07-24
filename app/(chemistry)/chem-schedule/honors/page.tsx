7ba59a718cd261c5da961d8aa1bdaf794ccbd70c
Chris <christophernavarrete00@gmail.com>
2024-07-24 14:02:55 -0700

    Trying to use env variables

--- app/(chemistry)/chem-schedule/honors/page.tsx
+++ app/(chemistry)/chem-schedule/honors/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
     title: 'Honors Chemistry Schedule',
   }
 const HonorsChemSchedulePage = () => {
-    const apiURL = process.env.NEXT_PUBLIC_HONORS_CHEMISTRY_API_URL || "https://script.google.com/macros/s/AKfycbwOIGu3J1f4WUvuurGWcQA57Mi3Tlv2sROBuSz2c-YqojUHoF_lmYepDZ4vQIZ0s-Vk1A/exec";
+    const apiURL = process.env.NEXT_PUBLIC_HONORS_CHEMISTRY_API_URL!;
     
 
     return(
--- app/(chemistry)/chem-schedule/regular/page.tsx
+++ app/(chemistry)/chem-schedule/regular/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
     title: 'Regular Chemistry Schedule',
   }
 const RegularChemSchedulePage = () => {
-    const apiURL = process.env.NEXT_PUBLIC_REGULAR_CHEMISTRY_API_URL || "https://script.google.com/macros/s/AKfycbwIhztXM518MhKrdP3zsqNhGH0W7M0LAVgzKjc-ETLYagK5thTAKGR0UMSIiX0X7z-8Dw/exec";
+    const apiURL = process.env.NEXT_PUBLIC_REGULAR_CHEMISTRY_API_URL!;
     
 
     return(
