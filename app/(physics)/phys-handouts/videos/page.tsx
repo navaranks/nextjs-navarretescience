6bcc4472bdf7ed33cf34f986a0dfd496907d4db5
navaranks <71299080+navaranks@users.noreply.github.com>
2024-08-18 11:11:51 -0700

    Added Video List to page

--- app/(physics)/phys-handouts/videos/page.tsx
+++ app/(physics)/phys-handouts/videos/page.tsx
@@ -1,15 +1,43 @@
-import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card'
-import React from 'react'
+import React from 'react';
+import HandoutTabs from '@/components/handout-tabs';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+import UnitTable from '@/components/handouts-table';
+import videosData from '@/public/physics/PhysVideoList.json';
+import { Metadata } from 'next';
 
-function PhysicsVideos() {
-  return (
-    <div className='flex flex-col px-4 gap-y-2'>
-      <Card className='max-w-screen-xl w-full mx-auto'>
-        <CardHeader className='text-center text-2xl py-2 px-4 font-extrabold'>Class Videos</CardHeader>
-        <CardDescription className='text-center pb-2'>Here is a list of videos that may be useful throughout the year!</CardDescription>
-      </Card>
-      </div>
-  )
+export const metadata: Metadata = {
+  title: 'Physics Videos',
 }
+const PhysicsVideos = () => {
+  const tabsData = (videosData as any[]).map(unit => ({
+    id: unit.id,
+    title: unit.title,
+    subtitle: unit.subtitle,
+    disabled: unit.disabled,
+    content: <UnitTable key={unit.id} unitTitle={unit.title} unitSubtitle={unit.subtitle} handouts={unit.handouts} />
+  }));
 
-export default PhysicsVideos
+  const borderColor = 'border-primary';
+
+  return (
+    <main className="flex flex-col px-4 gap-y-2">
+      <Card className="max-w-screen-xl w-full mx-auto">
+         <CardHeader className="text-center text-2xl py-2 px-4 font-extrabold">
+           Class Videos
+         </CardHeader>
+         <CardDescription className="text-center pb-2">
+           Here is a list of videos that may be useful throughout the year!
+         </CardDescription>
+       </Card>
+      <Card className="max-w-screen-xl w-full mx-auto">
+        <CardContent className='p-4'>
+          <HandoutTabs tabsData={tabsData} borderColor={borderColor} />
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default PhysicsVideos;
+
+
