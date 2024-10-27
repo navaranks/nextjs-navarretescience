50ca127ee86b646786720f1c166dee05826cc1e7
Chris <christophernavarrete00@gmail.com>
2024-10-27 16:25:57 -0700

    Updated Service Worker for Caching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,18 +3,22 @@ import HeroCard from "../../components/home-page/hero-card";
 import HomeCards from "../../components/home-page/home-cards";
 import IconDivider from "../../components/home-page/icon-divider";
 import AddToHomeScreen from "@/components/AddToHomeScreen";
+import RegisterServiceWorker from "@/components/home-page/RegisterServiceWorker";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full">
-      <div className="flex flex-col items-center p-6 bg-neutral-900 md:h-full h-fit">
-      <HeroCard />
-      <h1 className='mt-4 text-3xl text-center text-white text-balance'>Welcome to Mr. Navarrete's Class Website</h1>
-      <IconDivider />
-      <h3 className='mb-2 text-xl text-center text-white text-balance'>Select an icon to learn more about each class or the teacher!</h3>
-      <HomeCards />
-    </div>
-    <AddToHomeScreen />
-    </main>
+    <>
+      <RegisterServiceWorker />
+      <main className="h-screen w-full">
+        <div className="flex flex-col items-center p-6 bg-neutral-900 md:h-full h-fit">
+        <HeroCard />
+        <h1 className='mt-4 text-3xl text-center text-white text-balance'>Welcome to Mr. Navarrete's Class Website</h1>
+        <IconDivider />
+        <h3 className='mb-2 text-xl text-center text-white text-balance'>Select an icon to learn more about each class or the teacher!</h3>
+        <HomeCards />
+      </div>
+      <AddToHomeScreen />
+      </main>
+    </>
   );
 }
---
+++ components/home-page/RegisterServiceWorker.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+import useServiceWorker from "@/hooks/useServiceWorker";
+
+export default function RegisterServiceWorker() {
+   useServiceWorker();
+return null;
+}
